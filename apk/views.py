247ac90a5308d76a520b587e19e05f87b3b62788
Damian Banach <damian070799@wp.pl>
2022-11-07 13:06:34 +0100

    created endpoint to display userList

--- apk/views.py
+++ apk/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework import generics, permissions
 
-# Create your views here.
+
+User = get_user_model()
+
+
+class UserList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    query = User.objects.all()
+    serializer = UserCreateSerializer
+    name = 'user-list'
+
