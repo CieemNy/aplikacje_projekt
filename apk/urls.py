7023913336fb8af544a087cc49d01405663c82cc
Damian Banach <damian070799@wp.pl>
2022-11-07 13:56:09 +0100

    renamed urls

--- apk/urls.py
+++ apk/urls.py
@@ -6,5 +6,5 @@ from . import views
 admin.site.site_url = 'http://127.0.0.1:8000/apk/'
 urlpatterns = [
     path('users', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>', views.UserDetail.as_view(), name='user-detail')
+    path('users/<int:pk>', views.UserDetail.as_view(), name='user-detail'),
 ]
