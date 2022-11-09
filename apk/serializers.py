46f8371af965f45604c5ff2b02dd8932a8838b9f
Bartosz Gryciuk <f3ry39@gmail.com>
2022-11-09 19:39:51 +0100

    dodanie PostSerializer

--- apk/serializers.py
+++ apk/serializers.py
@@ -5,6 +5,15 @@ User = get_user_model()
 
 
 class UserCreateSerializer(UserCreateSerializer):
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta(UserCreateSerializer.Meta):
         model = User
         fields = ('id', 'email', 'username', 'name', 'surname', 'password')
+
+class PostSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+
+    class Meta:
+        model = Post
+        fields = ['id', 'user', 'title', 'content', 'addedDate']
