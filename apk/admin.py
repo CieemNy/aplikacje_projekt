c934641213c55974153851778afe6889a30168ca
Bartosz Gryciuk <f3ry39@gmail.com>
2022-11-09 19:36:52 +0100

    dodanie modelu Post do admin.py

--- apk/admin.py
+++ apk/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import UserAccount
+from .models import UserAccount, Post
 
 
 class UserAccountAdmin(admin.ModelAdmin):
     list_display = ['id', 'email', 'username', 'name', 'surname']
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', 'content', 'addedDate']
+
 
 admin.site.register(UserAccount, UserAccountAdmin)
+admin.site.register(Post, PostAdmin)
+
