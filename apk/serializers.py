29d8de11abc0bf9b27fe7b1145f05bd5ef2b6862
Damian Banach <damian070799@wp.pl>
2022-11-28 09:29:37 +0100

    created serializer: CommentSerializer

--- apk/serializers.py
+++ apk/serializers.py
@@ -8,15 +8,32 @@ User = get_user_model()
 
 class UserCreateSerializer(UserCreateSerializer):
     posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta(UserCreateSerializer.Meta):
         model = User
-        fields = ('id', 'email', 'username', 'name', 'surname', 'password', 'posts')
+        fields = ('id', 'email', 'username', 'name', 'surname', 'password', 'posts', 'comments')
 
 
 class PostSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Post
-        fields = ['id', 'user', 'title', 'content', 'addedDate']
+        fields = ['id', 'user', 'title', 'content', 'addedDate', 'comments']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    post = serializers.ReadOnlyField(source='post.title')
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id',
+            'post',
+            'user',
+            'content',
+            'addedDate',
+        ]
