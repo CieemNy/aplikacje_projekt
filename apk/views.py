9dc7f27aaefd78139da52472200bb55c58c9e7ec
Damian Banach <damian070799@wp.pl>
2022-11-28 09:20:45 +0100

    moved custom permissions to permissions.py

--- apk/views.py
+++ apk/views.py
@@ -2,31 +2,9 @@ from django.shortcuts import render
 from .models import *
 from .serializers import *
 from rest_framework import generics, permissions
-from rest_framework.permissions import SAFE_METHODS, BasePermission
+from .permissions import *
 
 User = get_user_model()
-
-
-# permission: only owner can edit post/comment
-
-class EditPermissions(BasePermission):
-    message = "Only owner of post/comment can editing post/comment"
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        return obj.user == request.user
-
-
-# permission: only owner can delete post/comment
-
-class DeletePermissions(BasePermission):
-    message = "Only owner of post/comment can delete post/comment"
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        return obj.user == request.user
 
 
 # endpoint: display list of users
